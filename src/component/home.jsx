8e60594113f6b82ea70984fdf448f266c6055d8f
Vaibhav Sharma <vaibhav13mn@gmail.com>
2023-09-14 02:46:59 +0530

    Updated 1

--- src/component/home.jsx
+++ src/component/home.jsx
@@ -17,10 +17,10 @@ export default class Home extends Component {
      {/* <h5 className='hh'>Student</h5> */}
       <h1 className="one">Hey! I AM</h1>
       <h1 id="o" className="one">VAIBHAV SHARMA</h1>
-      <p className="pp">Passionate Full Stack Developer who love to Code and Looking for an opportunity to apply my skills</p>
+      <p className="pp">Passionate Student who love to Code and Looking for an opportunity to apply my skills</p>
       
       <div className='mi'>
-      <a target="_blank" href='https://drive.google.com/file/d/1mLC4NGGDaGdsxscv_KgcZF2ZBj4LCOqF/view?usp=drive_link'><button id="dd" type="button" className="btn btn-warning bb">Download CV<FaDownload className='qwe'/></button></a>
+      <a target="_blank" rel="noreferrer" href='https://drive.google.com/file/d/1ZyHCzG1LpOiCPZPZ0kTcM2NiNZMYdPoh/view?usp=sharing'><button id="dd" type="button" className="btn btn-warning bb">Download CV<FaDownload className='qwe'/></button></a>
       
       <a href='mailto:vaibhav008mn@gmail.com'><button type="button" id="hire" className="btn btn-outline-warning bb">Hire me <FaAngleDoubleRight className='wwww' /></button></a>
       
@@ -32,25 +32,25 @@ export default class Home extends Component {
       
 
 
-      <a target="_blank" href="https://instagram.com/vaibhav.sharma13?igshid=MzNlNGNkZWQ4Mg==">
+      <a target="_blank" rel="noreferrer" href="https://instagram.com/vaibhav.sharma13?igshid=MzNlNGNkZWQ4Mg==">
       <FontAwesomeIcon icon={faInstagram} className='yuu'/>
       </a>
 
-      <a target="_blank" href="https://wa.me/9315233552?text=">
+      <a target="_blank" rel="noreferrer" href="https://wa.me/9315233552?text=">
       <FontAwesomeIcon icon={faWhatsapp} className='yuu'/>
       </a>
 
-      <a target="_blank" href="https://twitter.com/Vaibhav13mn?t=Ej0XBfJ2TnDFY2EJPUl8ZA&s=09">
+      <a target="_blank" rel="noreferrer" href="https://twitter.com/Vaibhav13mn?t=Ej0XBfJ2TnDFY2EJPUl8ZA&s=09">
       <FontAwesomeIcon icon={faTwitter} className='yuu'/>
       </a>
 
 
-      <a target="_blank" href="https://github.com/Mr-Vaibhav13">
+      <a target="_blank" rel="noreferrer" href="https://github.com/Mr-Vaibhav13">
       <FontAwesomeIcon icon={faGithub} className='yuu'/>
       </a>
 
 
-      <a target="_blank" href="https://www.linkedin.com/in/vaibhav-sharma-404088203/">
+      <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/vaibhav-sharma-404088203/">
       <FontAwesomeIcon icon={faLinkedinIn} className='yuu'/>
       </a>
 
